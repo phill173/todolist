5b57bc64a4a403936d52db8de93225fe7ac21fea
Phill Hayes <phillhayes6@hotmail.com>
2014-01-09 19:43:46 +0000

    add validations

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -21,6 +21,26 @@ describe "Creating todo lists"  do
 		expect(page).to have_content("New todo_list")
 
 
+		fill_in "Title", with: "Hi"
+		fill_in "Description", with: "This is my todo list"
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is my todo list")
+
+	end
+
+	it "displays an error when title less than three characters" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+
 		fill_in "Title", with: ""
 		fill_in "Description", with: "This is my todo list"
 		click_button "Create Todo list"
